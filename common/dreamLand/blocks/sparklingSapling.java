3bff0ea381a4718bd9b1721a7a0634ee9af658ce
Captain of all Shadows <captain_shadows@aol.com>
2013-04-08 00:09:49 -0400

    Fixed some comments

--- common/dreamLand/blocks/sparklingSapling.java
+++ common/dreamLand/blocks/sparklingSapling.java
@@ -9,9 +9,8 @@ public class sparklingSapling extends Item {
 
     public sparklingSapling(int par1) {
         super(par1);
-        // float f = 0.4F;
-        // this.setBlockBounds(0.5F - f, 0.0F, 0.5F - f, 0.5F + f, f * 2.0F,
-        // 0.5F + f);
+        /*float f = 0.4F;*/
+        /*this.setBlockBounds(0.5F - f, 0.0F, 0.5F - f, 0.5F + f, f * 2.0F, 0.5F + f);*/
         this.setCreativeTab(DreamLand.tabDreamLand);
     }
 
@@ -19,13 +18,29 @@ public class sparklingSapling extends Item {
     public void updateIcons(IconRegister par1IconRegister) {
         iconIndex = par1IconRegister.registerIcon(Archive.modID + ":sparklingSapling");
     }
+
     /*
-     * protected boolean canThisPlantGrowOnThisBlockID(int i) { return i ==
-     * DreamLand.dreamGrass.blockID; } public void updateTick(World world, int
-     * i, int j, int k, Random random) { int l = world.getBlockMetadata(i, j, k)
-     * & 3; world.setBlock(i, j, k, 0); Object obj = null; obj = new
-     * WorldGenSparklingTree(); if(!((WorldGenerator) (obj)).generate(world,
-     * random, i, j, k)) { world.setBlockMetadataWithNotify(i, j, k, blockID,
-     * l); } } public int damageDropped(int i) { return i & 3; }
+     * protected boolean canThisPlantGrowOnThisBlockID(int i) {
+     * return i == DreamLand.dreamGrass.blockID;
+     * }
      */
+
+    /*
+     * public void updateTick(World world, int i, int j, int k, Random random) {
+     * int l = world.getBlockMetadata(i, j, k) & 3;
+     * world.setBlock(i, j, k, 0);
+     * Object obj = null;
+     * obj = new WorldGenSparklingTree();
+     * if (!((WorldGenerator) (obj)).generate(world, random, i, j, k)){
+     * world.setBlockMetadataWithNotify(i, j, k, blockID, l);
+     * }
+     * }
+     */
+
+    /*
+     * public int damageDropped(int i) {
+     * return i & 3;
+     * }
+     */
+
 }
